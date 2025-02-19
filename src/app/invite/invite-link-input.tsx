cf6b83b37e9de10d9579aa91c6ee70a5001dab83
MatheusWesley <matheus.tarra@gmail.com>
2025-02-19 19:16:43 -0300

    Iniciando o projeto.

---
+++ src/app/invite/invite-link-input.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { InputField, InputIcon, InputRoot } from "@/components/Input";
+import { IconButton } from "@/components/icon-button";
+import { Copy, Link } from "lucide-react";
+
+interface InviteLinkInputProps {
+  inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink);
+  }
+
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+
+      <InputField readOnly defaultValue={inviteLink} />
+
+      <IconButton onClick={copyInviteLink} className="-mr-2">
+        <Copy className="size-5" />
+      </IconButton>
+    </InputRoot>
+  )
+}
